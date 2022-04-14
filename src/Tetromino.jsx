49557651bf647cc2053ad13e812eb66a7bab40e2
owtlaw6 <44997179+owtlaw6@users.noreply.github.com>
2022-04-14 15:14:08 +0300

    6:Draw a single tetromino on the board, in the center

---
+++ src/Tetromino.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { l } from './tetrominos'
+
+function Tetromino(){
+    const squares = [];
+    for(let i = 0; i < l.length; i++){
+        for(let j = 0; j < l[i].length; j++){
+            if(l[i][j] == true){
+                squares.push({row: i+1, col: j+1});
+            }
+        }
+    }
+    console.log(squares);
+    return( <>{squares.map (({row, col}) => 
+        (<div style={{gridRowStart: row, gridRowEnd: row+1, gridColumnStart: col, gridColumnEnd: col+1, backgroundColor: "red", }}> 
+        </div>))}</>
+    );
+}
+
+export default Tetromino
