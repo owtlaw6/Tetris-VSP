a151c1ee8960a5a73e3256a818155e69496d8c6d
owtlaw6 <44997179+owtlaw6@users.noreply.github.com>
2022-05-05 10:47:09 +0300

    9: When the tetromino disappears, i get a new one a the top
    refactor using an "ActiveTetro" class

--- src/classes/ActiveTetro.js
+++ src/classes/ActiveTetro.js
@@ -1,3 +1,46 @@
-class ActiveTetro{
-    
+import { randomTetromino } from "../tetrominos";
+import { DIRECTION } from "../utils/utils";
+
+export class ActiveTetro{
+    constructor(row = 0, column = 5){
+        this.currentPos = {
+            row, column
+        };
+        this.tetromino = randomTetromino();
+    }
+    forEachTile(callback){
+        this.tetromino.shape.forEach((row, rowIdx) => {
+            row.forEach((val, colIdx) => {
+                const row = this.currentPos.row + rowIdx;
+                const column = this.currentPos.column + colIdx;
+                if(val === true){
+                    callback(row, column, this.tetromino.color);
+                }
+            });
+        })
+    }
+    updatePosition(direction = DIRECTION.down){
+        let verticalAdjustment = 0;
+        if(direction === DIRECTION.down){
+            verticalAdjustment = 1;
+        }else if (direction === DIRECTION.up){
+            verticalAdjustment = -1;
+        }
+        this.currentPos.row += verticalAdjustment; 
+    }
+    eraseFrom(board){
+        this.forEachTile((row, column) => (board[row][column] = null));
+    }
+    checkCollision(board){
+        let isCollided = false;
+        this.forEachTile( (row, column) => {
+            if(row > 19 || column < 0 || row < 0 || column > 11 || board[row][column] != null){
+                isCollided = true;
+            }
+        })
+        return isCollided; 
+    }
+    drawOn(board){
+        this.forEachTile((row, column, color) => (board[row][column] = color));
+    }
 }
--- src/hooks/useBoard.js
+++ src/hooks/useBoard.js
@@ -1,90 +1,26 @@
 import { useEffect, useState, useRef, useCallback } from 'react';
+import { ActiveTetro } from '../classes/ActiveTetro';
 import { randomTetromino } from "../tetrominos";
-import { getEmptyBoard } from '../utils/utils';
-
-
-const DIRECTION = {
-    up: 'up',
-    down: 'down'
-}
+import { DIRECTION, getEmptyBoard } from '../utils/utils';
 
 export const useBoard = () => {
     const [board, setBoard] = useState(getEmptyBoard());
-    
-    const player = useRef({
-        currentPos: {row: 0, column: 5}, 
-        tetromino: randomTetromino()
-    });
+    const player = useRef(new ActiveTetro());
 
-    //const player = useRef(new ActiveTetro);
-
-    useEffect(() => {
-        //updateBoard();
-    }, [])
-
-    const updatePosition = useCallback((direction = DIRECTION.down) => {
-        let verticalAdjustment = 0;
-        if(direction === DIRECTION.down){
-            verticalAdjustment = 1;
-        }else if (direction === DIRECTION.up){
-            verticalAdjustment = -1;
-        }
-        player.current = {
-            currentPos:{ row: player.current.currentPos.row + verticalAdjustment, column: player.current.currentPos.column },
-            tetromino: player.current.tetromino
-        }
-    });
+    useEffect(() => { /*updateBoard();*/ }, [])    
 
     const updateBoard = () => {
+        player.current.eraseFrom(board);
+        player.current.updatePosition(DIRECTION.down);
 
-        player.current.tetromino.shape.forEach((row, rowIdx) => {
-            row.forEach((val, colIdx) => {
-                const row = player.current.currentPos.row + rowIdx
-                const column = player.current.currentPos.column + colIdx
-
-                if(val === true) {
-                  board[row][column] = null  
-                }
-                    
-            });
-        });
-
-        updatePosition(DIRECTION.down);
-
-        let isCollided = false;
-        player.current.tetromino.shape.forEach((row, rowIdx) => {
-            row.forEach((val, colIdx) => {
-                if(val === true){
-                    const row = player.current.currentPos.row + rowIdx
-                    const column = player.current.currentPos.column + colIdx
-                   
-                    if(row > 19 || row < 0 || column < 0 || column > 11 || board[row][column] !== null){ 
-                        isCollided = true;
-                    }
-                }
-            });
-        });
-
+        let isCollided = player.current.checkCollision(board);
         if(isCollided){
-            console.log('coliziune');
-            updatePosition(DIRECTION.up);
+            player.current.updatePosition(DIRECTION.up);
         }
-        player.current.tetromino.shape.forEach((row, rowIdx) => {
-            row.forEach((val, colIdx) => {
-                const row = player.current.currentPos.row + rowIdx
-                const column = player.current.currentPos.column + colIdx
-                if(val === true){
-                    board[row][column] = player.current.tetromino.color
-                }
-            });
-        });
+        player.current.drawOn(board);
 
         if(isCollided){
-            player.current = {
-                currentPos: {row: 0, column: 5}, 
-                tetromino: randomTetromino()
-            }
-            //player.current = new ActiveTetro;
+            player.current = new ActiveTetro();
         }
 
         setBoard([...board]);
@@ -92,3 +28,4 @@ export const useBoard = () => {
 
     return [updateBoard, board];
 }
+
